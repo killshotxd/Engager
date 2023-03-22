a1c1d72c9e002b5198529cb98d8ab955f1054cd7
killshotxd <hassanansari211@gmail.com>
2023-03-22 11:07:03 +0530

    user route complete

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ import { fileURLToPath } from "url";
 import multer from "multer";
 import { register } from "./controllers/auth.js";
 import authRoutes from "routes/auth.js";
+import userRoutes from "routes/users.js";
 
 // CONFIGURATIONS
 
@@ -45,6 +46,7 @@ app.post("auth/register", upload.single("picture", register));
 // ROUTES
 
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 // MONGOOSE
 const PORT = process.env.PORT || 6001;
